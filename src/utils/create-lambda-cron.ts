3b8bb984ef334d9d5b4c0f897f31627afe33bd0d
Praveen Kumar <praveen@Praveens-MacBook-Pro.local>
2021-08-27 09:59:41 +0530

    Fixed linting errors

--- src/utils/create-lambda-cron.ts
+++ src/utils/create-lambda-cron.ts
@@ -5,25 +5,27 @@ import { Stack } from '@aws-cdk/core';
 import { Vpc } from '@aws-cdk/aws-ec2';
 
 interface ILambdaCron {
-    stack: Stack,
-    vpc?: Vpc,
-    id: string,
-    lambdaProps: lambda.FunctionProps,
-    eventRuleProps: events.RuleProps,
-};
+  stack: Stack;
+  vpc?: Vpc;
+  id: string;
+  lambdaProps: lambda.FunctionProps;
+  eventRuleProps: events.RuleProps;
+}
 
 export const createLambdaCron = ({
-    stack,
-    vpc,
-    id,
-    lambdaProps,
-    eventRuleProps,
+  stack,
+  vpc,
+  id,
+  lambdaProps,
+  eventRuleProps,
 }: ILambdaCron): void => {
-    try {
-        const newLambda = new lambda.Function(stack, id, { vpc, ...lambdaProps });
-        const newEventRule = new events.Rule(stack, `${id}_rule`, { ...eventRuleProps });
-        newEventRule.addTarget(new targets.LambdaFunction(newLambda));
-    } catch (error) {
-        throw new Error(error);
-    }
-}
+  try {
+    const newLambda = new lambda.Function(stack, id, { vpc, ...lambdaProps });
+    const newEventRule = new events.Rule(stack, `${id}_rule`, {
+      ...eventRuleProps,
+    });
+    newEventRule.addTarget(new targets.LambdaFunction(newLambda));
+  } catch (error) {
+    throw new Error(error);
+  }
+};
